b3eca1cfcdcde840c991349c7f1c6fdc0c035a7e
root <root@7800x3dpc>
2024-07-21 13:09:00 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CFlat
 CFlat language, based on C syntax. The compiler does not use LLVM for assembly generation, but is still very optimized. The compiler is not meant to optimize bad coding practices however.
+# Requirements
+x86\_64 machine
+NASM assembler
--- code.asm
+++ code.asm
@@ -1,53 +1,33 @@
 section .data
 section .rodata
-  FI0  dd  1065353216
 section .bss
 section .text
 global _start
 _start:
   push rbp
   mov rbp, rsp
-  mov DWORD [rbp-16], 1065353216
-  mov DWORD [rbp-12], 1073741824
-  movss xmm0, DWORD [rbp-16]
-  subss xmm0, DWORD [FI0]
-  movss xmm1, xmm0
-  movss xmm0, DWORD [rbp-16]
-  addss xmm0, DWORD [FI0]
-  ucomiss xmm0, xmm1
-  jp .L0
-  ucomiss xmm0,xmm1
-  jne .L0
-  mov DWORD [rbp-16], 1084227584
+  mov QWORD [rbp-0], 15
+  mov rax, QWORD [rbp-0]
+  shl rax, 3
+  add rax, 10
+  mov QWORD [rbp-0], rax
+  mov rax, QWORD [rbp-0]
+  add rax, 1
+  test rax, rax
+  jz .L0
+  mov rax, QWORD [rbp-0]
+  add rax, 1
+  mov QWORD [rbp-0], rax
+  jmp .L2
 .L0:
-  mov DWORD [rbp-8], 118837123
-  movss xmm0, DWORD [rbp-16]
-  comiss xmm0, DWORD [rbp-12]
-  jb .L3
-  mov DWORD [rbp-16], -1082130432
-.L3:
-  mov DWORD [rbp-4], 11
-  movss xmm0, DWORD [rbp-16]
-  comiss xmm0, DWORD [rbp-12]
-  jnb .L7
-  movss xmm1, DWORD [rbp-12]
-  comiss xmm1, DWORD [rbp-16]
-  jbe .L6
-.L7:
-  mov DWORD [rbp-16], 1211860160
-.L6:
-  mov DWORD [rbp-0], 1073741824
-  movss xmm0, DWORD [rbp-12]
-  subss xmm0, DWORD [FI0]
-  movss xmm1, xmm0
-  movss xmm0, DWORD [rbp-12]
-  addss xmm0, DWORD [FI0]
-  ucomiss xmm0, xmm1
-  jp .L9
-  ucomiss xmm0,xmm1
-  jne .L9
-  mov DWORD [rbp-12], 1073741824
-.L9:
+  mov rax, QWORD [rbp-0]
+  neg rax
+  mov QWORD [rbp-0], rax
+.L2:
+  mov rax, QWORD [rbp-0]
+  mov rdi, rax
+  mov rax, 60
+  syscall
   pop rbp
   mov rax, 60
   xor rdi, rdi
