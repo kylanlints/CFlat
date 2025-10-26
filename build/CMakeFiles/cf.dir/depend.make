7eac3d237c7abc29654cc09187351755343d4d09
Kylan Lints <kylanlints@gmail.com>
2025-10-26 15:06:26 -0700

    Delete build directory

--- build/CMakeFiles/cf.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cf.
-# This may be replaced when dependencies are built.
--- build/include/CMakeFiles/fmt.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libfmt.a"
-  "libfmt.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fmt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
